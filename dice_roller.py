6dbdcd2ba87d5835976145efdfa3c945ab50e776
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2025-09-15 19:36:57 +0000

    added  a json file to recored past rolls

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,14 @@
 import random as r
+import json
+data = {}
+with open('pastRolls.json', 'r') as f:
+    data = json.load(fp=f)
 def roll_dice(sides):
     roll = r.randint(1,sides)
     return roll
 sides = int(input('How many sides are on your dice?(enter an interger > 1)\n>'))
 roll = roll_dice(sides=sides)
-print(roll)
+print(f'That was roll {len(data)} and you rolled an {roll}')
+data[len(data)] = roll
+with open('pastRolls.json', 'w') as f:
+    json.dump(data, fp=f, indent=4)
